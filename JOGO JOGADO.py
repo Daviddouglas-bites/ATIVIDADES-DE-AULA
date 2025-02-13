2bdad65ac5a53fde2fc5c76274aad75004b42348
David Douglas Santos Barbosa <d4v1d.soudouglas@gmail.com>
2025-02-12 21:01:50 -0300

    Update JOGO JOGADO.py

--- JOGO JOGADO.py
+++ JOGO JOGADO.py
@@ -2,10 +2,37 @@ import pygame
 import random
 
 # Definindo cores
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-PINK = (255, 192, 203)
+colors = {
+    "Red": (255, 0, 0),
+    "Green": (0, 255, 0),
+    "Blue": (0, 0, 255),
+    "Yellow": (255, 255, 0),
+    "Cyan": (0, 255, 255),
+    "Magenta": (255, 0, 255),
+    "Orange": (255, 165, 0),
+    "Purple": (128, 0, 128),
+    "Brown": (165, 42, 42),
+    "Pink": (255, 105, 180),
+    "Gray": (128, 128, 128),
+    "Lime": (0, 255, 0),
+    "Teal": (0, 128, 128),
+    "Olive": (128, 128, 0),
+    "Slate Blue": (106, 90, 205),
+    "Gold": (255, 215, 0),
+    "Lavender": (230, 230, 250),
+    "Peach": (255, 218, 185),
+    "Mint": (189, 252, 201),
+    "Light Pink": (255, 182, 193),
+    "Sky Blue": (135, 206, 235),
+    "Light Yellow": (255, 255, 224),
+    "Light Green": (144, 238, 144),
+    "Dark Gray": (169, 169, 169),
+    "Light Gray": (211, 211, 211),
+    "Silver": (192, 192, 192),
+    "Dim Gray": (105, 105, 105),
+}
+
+
 
 # Inicializando o Pygame
 pygame.init()
@@ -55,7 +82,7 @@ def gerar_labirinto():
 def desenhar_labirinto(labirinto):
     for y in range(LINHAS):
         for x in range(COLUNAS):
-            cor = WHITE if labirinto[y][x] == 0 else BLACK
+            cor = PINK if labirinto[y][x] == 0 else BLACK
             pygame.draw.rect(tela, cor, (x * TAMANHO_CELULA, y * TAMANHO_CELULA, TAMANHO_CELULA, TAMANHO_CELULA))
 
 # Loop principal do jogo
